2d24109436887cc5f70c31f70a4adf8d4d446f0e
Aryan arya <144515708+aryanabhiarya8@users.noreply.github.com>
2023-12-29 03:33:31 +0530

    For Misty_Music

--- DAXXMUSIC/plugins/misc/autoleave.py
+++ DAXXMUSIC/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001802990747
-                                and i.chat.id != -1001802990747
+                                and i.chat.id != --1001835969605
+                                and i.chat.id != --1001835969605
                             ):
                                 if left == 20:
                                     continue
